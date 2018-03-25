e17fc274f74b73177a52cbf8752e67d71fc970e6
implicitdef <implicitdef@users.noreply.github.com>
2018-03-25 10:14:11 +0200

    Added setup of basic interaction between kotlin and js

--- javascript/README.md
+++ javascript/README.md
@@ -6,10 +6,13 @@ React app with create-react-app.
 # How to
 
 
+(before all other tasks, to rebuild the Kotlin code and move it over to public/)
+
+    npm run kotlin
+
 To run locally
 
     npm start
-
 
 To build
 
--- javascript/src/index.js
+++ javascript/src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+window.KotlinSim.triggerUserEvent({name: "phat", number:345})
+
+
+
+window.KotlinSim.triggerCallback((world) => {
+  console.log('in the callback', world);
+});
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
